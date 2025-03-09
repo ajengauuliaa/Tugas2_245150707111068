a5db0d116d295da04727ce4708fb561ee9c3571a
ajengauuliaa <ajengauliaa7@gmail.com>
2025-03-09 21:15:52 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        //menggunakan default constructor
+        Pegawai pegawai1 = new Pegawai();
+        pegawai1.displayInfo();
+        System.out.println("========================");
+
+        //menggunakan overloading constructor
+        Pegawai pegawai2 = new Pegawai("Tawan", "Manager", 5000000, 1500000, 500000, 100000);
+        pegawai2.displayInfo();
+
+        //menambah bonus dan menghitung ulang
+        System.out.println("\nMenambahkan Bonus...");
+        pegawai2.tambahBonus(100000);
+        pegawai2.displayInfo();
+    }
+}
 
